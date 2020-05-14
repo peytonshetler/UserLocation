ba934fc1612b572cf2599dd277d4de8f59f47a72
Peyton Shetler <peyton.shetler@gmail.com>
2020-05-14 11:30:18 -0500

    Initial Commit

---
+++ UserLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLocation
+//
+//  Created by Peyton Shetler on 5/14/20.
+//  Copyright © 2020 Peyton Shetler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
